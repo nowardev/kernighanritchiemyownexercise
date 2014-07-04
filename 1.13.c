fd7981321d95317112c3c2e01a218f88344acc6b
nowardev <nowardev@gmail.com>
2014-07-04 16:54:09 +0200

    better 1.13

--- 1.13.c
+++ 1.13.c
@@ -1,9 +1,9 @@
-// nowardev print number  white space and other char 
+// nowardev histogram frequency char
  
 #include <stdio.h>
 main(){
 	int i,j, c, max, maxtag;
-	int ndigit[97]; 
+	int ndigit[97]; //just normal char 
 	max = 0 ; 
 	for (i = 0 ; i <= 96 ; ++i ){
 	 ndigit[i] = 0;
@@ -14,7 +14,7 @@ main(){
 			j=0 ;
 			for (i = 32 ; i <=127 ; ++i ){
 				 
-				if (i == c ){
+				if (i == c ){ //every char it's a number so we can test in this way 
 					++ndigit[j];
 				}
 				++j; 
@@ -25,7 +25,7 @@ main(){
 
 
  
-// find max 
+// we need to find max  to draw a histogram 
 
 		for (i = 0 ; i <=96 ; ++i ){
 			if (max <= ndigit[i]){
@@ -33,7 +33,7 @@ main(){
 				maxtag = i ; 
 			}
 		}
-printf("\n Max value is : %d for char %c \n", max , maxtag );
+printf("\n Max value is : %d for char \"%c\" \n", max , maxtag );
 	
 	for (i = max ; i >= 0 ; --i ){
 		//print first line
@@ -56,17 +56,19 @@ printf("\n Max value is : %d for char %c \n", max , maxtag );
 		printf("_"  );
 		}
 	printf("\n");
-
+//print char
 	for (i = 32 ; i <= 127 ; ++i ){
 		printf("%c", i  );
 		}
 
 		printf("\n");
+		printf("\n");
 	j=0; 
+	printf("char:\tfrequency:\n" );
 	for (i = 32 ; i <= 127 ; ++i ){
 		
 		if (ndigit[j] !=0 ){
-		printf("char %c frequency=  %d \n", i,  ndigit[j] );
+			printf("%c\t%d \n", i,  ndigit[j] );
 			
 		}
 		++j;
